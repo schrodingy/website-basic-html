89b95949039ffd8cd1e803daef50c9679ba08867
Deven Mithal <devenmithal@gmail.com>
2019-07-15 21:37:35 -0500

    Boxes

--- canvas.js
+++ canvas.js
@@ -2,3 +2,9 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 console.log(canvas);
+
+var c; // context
+c = canvas.getContext('2d');
+c.fillRect(100, 100, 100, 100);
+c.fillRect(200, 200, 100, 100);
+c.fillRect(300, 300, 100, 100);
