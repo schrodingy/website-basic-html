ed5ccd707517f63fe9cc106129a31d7b5deba955
Deven Mithal <devenmithal@gmail.com>
2019-07-16 19:49:23 -0500

    Drawing lines

--- canvas.js
+++ canvas.js
@@ -8,3 +8,10 @@ c = canvas.getContext('2d');
 c.fillRect(100, 100, 100, 100);
 c.fillRect(200, 200, 100, 100);
 c.fillRect(300, 300, 100, 100);
+
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = "#fa34a3";
+c.stroke();
