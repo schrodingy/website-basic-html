d8adbe459b2647885fd61adc8d9f91802f7a53cd
Deven Mithal <devenmithal@gmail.com>
2019-07-16 20:14:18 -0500

    Drawing arcs

--- canvas.js
+++ canvas.js
@@ -5,8 +5,11 @@ console.log(canvas);
 
 var c; // context
 c = canvas.getContext('2d');
+c.fillStyle = "rgba(255, 0, 0, 0.5)"
 c.fillRect(100, 100, 100, 100);
+c.fillStyle = "rgba(100, 0, 0, 0.5)"
 c.fillRect(200, 200, 100, 100);
+c.fillStyle = "rgba(5, 0, 0, 0.5)"
 c.fillRect(300, 300, 100, 100);
 
 c.beginPath();
@@ -14,4 +17,9 @@ c.moveTo(50, 300);
 c.lineTo(300, 100);
 c.lineTo(400, 300);
 c.strokeStyle = "#fa34a3";
+c.stroke();
+
+c.beginPath();
+c.arc(300, 300, 30, Math.PI, -Math.PI, false);
+c.strokeStyle = "blue";
 c.stroke();
